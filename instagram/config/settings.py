7c975067ab7c680a6b76ff613f7691bf84360055
2siwon <lee89640245@gmail.com>
2017-10-13 16:39:31 +0900

    upload_to가 동작하도록 MEDIA_ROOT를 설정

--- instagram/config/settings.py
+++ instagram/config/settings.py
@@ -20,6 +20,11 @@ ROOT_DIR = os.path.dirname(BASE_DIR)
 # instagram_project/.config_secret/
 CONFIG_SECRET_DIR = os.path.join(ROOT_DIR, '.config_secret')
 
+#instagram_project/instagram/media/
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#instagram_project/instagram/static/
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 f = open(os.path.join(CONFIG_SECRET_DIR, "settings_common.json"), 'r')
 config_secret_common_str = f.read()
 f.close()
--- instagram/post/models.py
+++ instagram/post/models.py
@@ -2,13 +2,16 @@ from django.db import models
 
 
 # Create your models here.
+from config.settings import MEDIA_ROOT
+
 
 class Post(models.Model):
-    photo = models.ImageField(upload_to='photo')
+    photo = models.ImageField(upload_to='post')
+    # setting.py에
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-class PostComment:
+class PostComment(models.Model):
     post = models.ForeignKey(Post)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
