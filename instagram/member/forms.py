f31d93976be34ed5cc78bea51090de73b195af94
2siwon <lee89640245@gmail.com>
2017-10-18 00:58:36 +0900

    username field custom validation

--- instagram/member/forms.py
+++ instagram/member/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class SignupForm(forms.Form):
@@ -10,4 +13,19 @@ class SignupForm(forms.Form):
             }
         ),
     )
-    password = forms.PasswordInput()
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control'
+            }
+        ),
+    )
+
+    # clean_<field_name>
+    def clean_username(self):
+        data = self.cleaned_data['username']
+        # 유저가 존재하면 faorms.ValidationError를 발생
+        # 아니면 data를 리턴
+        if User.objects.filter(username=data).exists():
+            raise forms.ValidationError('Username already exists!')
+        return data
--- instagram/member/views.py
+++ instagram/member/views.py
@@ -3,24 +3,37 @@ from django.shortcuts import render
 
 from django.contrib.auth import get_user_model
 
+from .forms import SignupForm
+
 User = get_user_model()  # helper 함수
 
 
 def signup(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        # 데이터가 binding 된 SignupForm 인스턴스를 생성
+        form = SignupForm(request.POST)
+        # 해당 form 이 자신의 필드에 유효한 데이터를 가지고 있는지 유효성 검사
+        if form.is_valid():
+            # 통과한 경우 정제된 데이터 (cleaned_data)에서 username 과 password 항목을 가져옴
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
 
-        if username and password:
-            # 이미 username항목이 주어진 username값으로 존재하는 User가 있는지 검사
-            if User.objects.filter(username=username).exists():
-                return HttpResponse(f'Username {username} is already exist')
-            # username, password가 주어졌고 중복되는 User가 없다면 User생성
+            # username, password 가 주어졌고 중복되는 User 가 없다면 User 생성
             user = User.objects.create_user(
                 username=username,
                 password=password,
             )
 
             return HttpResponse(f'{user.username }, { user.password }')
+        print(form.cleaned_data)
+        print(form.errors)
 
-    return render(request, 'member/signup.html')
+    # GET 요청시 SignupForm 인스턴스를 signup_form 변수에 할당, context 에 같은 키/값으로 전달
+    else:
+        form = SignupForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'member/signup.html', context)
