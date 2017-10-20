287b6b3ec7d0ba9305cd1bfc7394dfc3399f2831
2siwon <lee89640245@gmail.com>
2017-10-20 10:30:08 +0900

    작성자 헤더에 표시, post_list.html, post_detail.html 리팩토링, post,comment삭제기능추가

--- instagram/member/urls.py
+++ instagram/member/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^member/signup/$', views.signup, name='signup'),
-    url(r'^member/login/$', views.login, name='login'),
-    url(r'^member/logout/$', views.logout, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
 
 ]
--- instagram/post/views.py
+++ instagram/post/views.py
@@ -6,12 +6,6 @@ from .models import Post, PostComment
 
 
 def post_list(request):
-    """
-    모든 Post목록을 리턴
-    template은 'post/post_list.html'을 사용
-    :param request:
-    :return:
-    """
     posts = Post.objects.exclude(author__isnull=True)
     comment_form = CommentForm()
 
@@ -23,16 +17,25 @@ def post_list(request):
     return render(request, 'post/post_list.html', context)
 
 
-def post_create(request):
-    """
-    1. 이 뷰에 접근할 때, 해당 사용자가 인증된 상태가 아니면 로그인 뷰로 redirect
-    2. form.is_valid() 를 통과한 후, 생성하는 Post객체에 author정보를 추가
-    :param request:
-    :return:
-    """
+def post_detail(request, post_pk):
+    # post = Post.objects.get(pk=post_pk)
 
+    # Post 에서 pk값을 찾고 없으면 404 error
+    post = get_object_or_404(Post, pk=post_pk)
+    comment_form = CommentForm()
+
+    context = {
+        'post': post,
+        'comment_form': comment_form,
+    }
+
+    return render(request, 'post/post_detail.html', context)
+
+
+def post_create(request):
+    # 로그인한 유저만 접근 및 POST요청 가능
     if not request.user.is_authenticated:
-        redirect('member:login')
+        return redirect('member:login')
 
     if request.method == 'POST':
         # POST요청의 경우 PostForm인스턴스 생성과정에서 request.POST, request.FILES를 사용
@@ -44,7 +47,7 @@ def post_create(request):
                 author=request.user,
                 photo=form.cleaned_data['photo'],
             )
-            return HttpResponse(f'<img src="{post.photo.url}">')
+            return redirect('post:post_list')
 
     else:
         # GET 요청의 경우 빈 PostForm인스턴스를 생성해서 템플릿에 전달
@@ -58,22 +61,9 @@ def post_create(request):
     return render(request, 'post/post_create.html', context)
 
 
-def post_detail(request, post_pk):
-    # post = Post.objects.get(pk=post_pk)
-
-    # Post 에서 pk값을 찾고 없으면 404 error
-    post = get_object_or_404(pk=post_pk)
-    comment_form = CommentForm()
-
-    context = {
-        'post': post,
-        'comment_form': comment_form,
-    }
-
-    return render(request, 'post/post_detail.html', context)
-
-
 def comment_create(request, post_pk):
+    if not request.user.is_authenticated:
+        return redirect('member:login')
     # URL get parameter로 온 'post_pk'에 해당하는
     # Post instacne를 'post'변수에 할당
     # 찾지못하면 404Error를 브라우저에 리턴
@@ -85,6 +75,7 @@ def comment_create(request, post_pk):
         if form.is_valid():
             # 통과한 경우, Post에 해당하는 Comment 인스턴스 생성
             PostComment.objects.create(
+                author=request.user,
                 post=post,
                 content=form.cleaned_data['content']
             )
@@ -97,3 +88,17 @@ def comment_create(request, post_pk):
                 return redirect(next)
             # 생성 후 Post의 detail화면으로 이동
             return redirect('post:post_detail', post_pk=post_pk)
+
+
+def post_delete(request, author_pk, post_pk):
+    if request.user.is_authenticated:
+        post = Post.objects.filter(author_id=author_pk).get(pk=post_pk)
+        post.delete()
+    return redirect('post:post_list')
+
+
+def comment_delete(request, author_pk, comment_pk):
+    if request.user.is_authenticated:
+        comment = PostComment.objects.filter(author_id=author_pk).get(pk=comment_pk)
+        comment.delete()
+    return redirect('post:post_list')
